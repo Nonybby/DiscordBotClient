64247b431ae9de8603d7a06f865b99b35415d79b
Elysia <71698422+aiko-chan-ai@users.noreply.github.com>
2025-07-07 23:24:55 +0700

    chore: fix

--- scripts/cloneDiscordProtos.js
+++ scripts/cloneDiscordProtos.js
@@ -6,6 +6,7 @@ const { execSync } = require('child_process');
 (async () => {
 	const cloneDir = path.join('.', 'DiscordProtos');
 	console.log('> Clone discord-userdoccers/discord-protos.git...');
+	fs.rmSync(cloneDir, { recursive: true, force: true });
 	await clone(
 		'https://github.com/discord-userdoccers/discord-protos.git',
 		cloneDir,
@@ -23,4 +24,15 @@ const { execSync } = require('child_process');
 		cwd: cloneDir,
 	});
 	console.log('> discord-protos dependencies installed.');
+	// Remove "dist" from ".gitignore" file
+	const gitignorePath = path.join(cloneDir, '.gitignore');
+	if (fs.existsSync(gitignorePath)) {
+		const gitignoreContent = fs.readFileSync(gitignorePath, 'utf8');
+		const updatedContent = gitignoreContent
+			.split('\n')
+			.filter(line => !line.includes('dist'))
+			.join('\n');
+		fs.writeFileSync(gitignorePath, updatedContent);
+		console.log('> Updated .gitignore to remove "dist" entry.');
+	}
 })();
